0e46270073a9d51357050d70dd6d94fb1179a15f
Waldo <w86ccompositor@gmail.com>
2017-08-27 15:38:10 -0400

    Added a MusicGenres Controller with no actions yet

---
+++ app/controllers/musicgenres_controller.rb
@@ -0,0 +1,2 @@
+class MusicgenresController < ApplicationController
+end
---
+++ test/controllers/musicgenres_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MusicgenresControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
