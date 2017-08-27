50ec5c40febbc3b743384a42bd2c677e369152ad
Waldo <w86ccompositor@gmail.com>
2017-08-26 20:07:17 -0400

    Photobooth Model migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826202717) do
+ActiveRecord::Schema.define(version: 20170827000326) do
+
+  create_table "photobooths", force: :cascade do |t|
+    t.string "filename"
+    t.string "content_type"
+    t.binary "file_content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photobooths_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
