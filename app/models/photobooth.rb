5065c2ba0ba914822ee1c740c7be41c7646fb055
Waldo <w86ccompositor@gmail.com>
2017-08-28 00:34:34 -0400

    Added the filters to the Photobooth model from Paperclip and remove the old vanilla code to upload an image

--- app/models/photobooth.rb
+++ app/models/photobooth.rb
@@ -2,16 +2,7 @@ class Photobooth < ApplicationRecord
   belongs_to :user,  optional: true
   #validates_presence_of :user
 
-      def initialize(params = {})
-        # File is now an instance variable so it can be
-          # accessed in the validation.
-      @file = params.delete(:file)
-    super
-      if @file
-        self.filename = sanitize_forbidden_attributes(@file.original_filename)
-        self.content_type = @file.content_type
-        self.file_content= file_content
-      end
-    end
+    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
