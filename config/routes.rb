4753ebeacf70b43f46133bb073bb535e6ca9b296
Waldo <w86ccompositor@gmail.com>
2017-08-29 14:31:10 -0400

    Added important comments for some routes, read them before changing any route please

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
-
-  get 'social/index'
-
-  post 'musicgenres/index' => "musicgenres#index", as: :musicgenres
-
-  get 'timeline/index'
-
-
-  authenticated :user do
-
-      root to: 'users#profile', as: :authenticated_root
-  end
-
-  root 'home#index'
-
   devise_for :users
 
-  resources :photobooths do
+# The users that are authenticated will be redirected to users profile after sign in, DO NOT CHANGE THE LINE OF CODE BELOW
+  authenticated :user do
+     root to: 'users#profile', as: :authenticated_root
   end
-  resources :musicgenres, only: [:index]
 
+  # The users that are not authenticated will be redirected to home index, DO NOT CHANGE THE LINE OF CODE BELOW
+  root 'home#index'
+
+
+resources :photobooths
+resources :musicgenres, only: [:index]
+
+  get 'social/index'
+  get 'timeline/index'
+
+  post 'musicgenres/index' => "musicgenres#index", as: :musicgenres
   get 'musicgenres/:id' => "musicgenres#show"
+ 
+
 end
