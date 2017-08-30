0d1bdfc2cdeed2f30cffdcc114b218329c44c1c3
Waldo <w86ccompositor@gmail.com>
2017-08-29 20:46:08 -0400

    The music genres array works perfect

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,26 +1,278 @@
-GENRES = {
-  "latin": [
-    {
-      name: 'Song Name',
-      url: 'AWS URL'
-    },
-    {
-      name: 'Song Name',
-      url: 'AWS URL'
-    },
-    {
-      name: 'Song Name',
-      url: 'AWS URL'
-    }
-  ],
-  "jaz": [
-    {
-      name: 'Song Name',
-      url: 'AWS URL'
-    },
-    {
-      name: 'Song Name',
-      url: 'AWS URL'
-    }
-  ]
+genres = {
+
+  'rock': [
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+      ],
+
+  'jazz': [
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+      ],
+
+  'pop':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+      ],
+
+  'country':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'latin':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'hip_hop':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'classic':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'contemporary':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'electronic_dance':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'rb':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'indie':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
+
+  'soul':[
+        {
+          name: 'track1',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track2',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track3',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track4',
+          url: 'AWS URL'
+        },
+        {
+          name: 'track5',
+          url: 'AWS URL'
+        }
+    ],
 }
